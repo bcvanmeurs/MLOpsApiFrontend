80c5228259f5cda16272d1a552a2390c6e30791e
bcvanmeurs <bcvanmeurs@users.noreply.github.com>
2020-12-28 14:42:57 +0100

    working dropdowns

--- src/Main.elm
+++ src/Main.elm
@@ -9,6 +9,7 @@ import Html.Events exposing (onClick, onInput)
 -- MAIN
 
 
+main : Program () Model Msg
 main =
     Browser.sandbox { init = init, update = update, view = view }
 
@@ -18,13 +19,24 @@ main =
 
 
 type alias Model =
-    { content : String
+    { buying : String
+    , doors : String
+    , lug_boot : String
+    , maint : String
+    , persons : String
+    , safety : String
     }
 
 
 init : Model
 init =
-    { content = Maybe.withDefault "no data found" (List.head options) }
+    { buying = "low"
+    , maint = "low"
+    , doors = "2"
+    , persons = "2"
+    , lug_boot = "small"
+    , safety = "low"
+    }
 
 
 
@@ -32,14 +44,34 @@ init =
 
 
 type Msg
-    = Test String
+    = Buying String
+    | Maint String
+    | Doors String
+    | Persons String
+    | Lug_boot String
+    | Safety String
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Test selected ->
-            { model | content = selected }
+        Buying selected ->
+            { model | buying = selected }
+
+        Maint selected ->
+            { model | maint = selected }
+
+        Doors selected ->
+            { model | doors = selected }
+
+        Persons selected ->
+            { model | persons = selected }
+
+        Lug_boot selected ->
+            { model | lug_boot = selected }
+
+        Safety selected ->
+            { model | safety = selected }
 
 
 
@@ -49,14 +81,49 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ select [ onInput Test ]
-            (getOptions options)
-        , div [] [ text model.content ]
+        [ div []
+            [ div []
+                [ text model.buying
+                , select [ onInput Buying ] (getOptions factors.buying)
+                ]
+            , div []
+                [ text model.maint
+                , select [ onInput Maint ] (getOptions factors.maint)
+                ]
+            , div []
+                [ text model.doors
+                , select [ onInput Doors ] (getOptions factors.doors)
+                ]
+            , div []
+                [ text model.persons
+                , select [ onInput Persons ] (getOptions factors.persons)
+                ]
+            , div []
+                [ text model.lug_boot
+                , select [ onInput Lug_boot ] (getOptions factors.lug_boot)
+                ]
+            , div []
+                [ text model.safety
+                , select [ onInput Safety ] (getOptions factors.safety)
+                ]
+            ]
         ]
 
 
-options =
-    [ "unacceptable", "acceptable", "good", "very good" ]
+factors =
+    { buying = [ "low", "med", "high", "vhigh" ]
+    , maint = [ "low", "med", "high", "vhigh" ]
+    , doors = [ "2", "3", "4", "5more" ]
+    , persons = [ "2", "4", "5more" ]
+    , lug_boot = [ "small", "med", "big" ]
+    , safety = [ "low", "med", "high" ]
+    }
+
+
+
+-- generateSelect : String -> List String -> Html Msg
+-- generateSelect string list =
+--     select [ onInput Test ] (getOptions factors.buying)
 
 
 getOptions : List String -> List (Html msg)
