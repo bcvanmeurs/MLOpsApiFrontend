97be2347baa4fc1c4d8ab2b345722bde707c23c2
bcvanmeurs <bcvanmeurs@users.noreply.github.com>
2020-12-28 12:29:24 +0100

    auto format

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,7 @@
 module Main exposing (..)
 
--- Press buttons to increment and decrement a counter.
---
--- Read how it works:
---   https://guide.elm-lang.org/architecture/buttons.html
---
-
-
 import Browser
-import Html exposing (Html, button, div, text, select, option)
+import Html exposing (Html, button, div, option, select, text)
 import Html.Events exposing (onClick, onInput)
 
 
@@ -17,35 +10,36 @@ import Html.Events exposing (onClick, onInput)
 
 
 main =
-Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
 
 
 
 -- MODEL
 
+
 type alias Model =
-  { content : String
-  }
+    { content : String
+    }
 
 
 init : Model
 init =
-  { content = Maybe.withDefault "no data found" (List.head options) }
+    { content = Maybe.withDefault "no data found" (List.head options) }
+
 
 
 -- UPDATE
 
 
 type Msg
-  = Test String
+    = Test String
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Test selected ->
-      { model | content = selected }
-
+    case msg of
+        Test selected ->
+            { model | content = selected }
 
 
 
@@ -54,17 +48,17 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ select [onInput Test] 
-      (getOptions options)
-    , div [][text model.content]
-    
-    ]
-    
+    div []
+        [ select [ onInput Test ]
+            (getOptions options)
+        , div [] [ text model.content ]
+        ]
 
-options = ["unacceptable", "acceptable", "good", "very good"]
---log (transform options
-    
-getOptions : List String -> List(Html msg)
+
+options =
+    [ "unacceptable", "acceptable", "good", "very good" ]
+
+
+getOptions : List String -> List (Html msg)
 getOptions list =
-  List.map (\item -> option [][text item] ) list
+    List.map (\item -> option [] [ text item ]) list
